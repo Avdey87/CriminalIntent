f6d65d9df01fc372509eb6707936586d0ca3f256
avdey <avdey87@gmail.com>
2017-10-17 15:03:28 +0300

    обновление фото

--- app/src/main/java/com/aavdeev/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/aavdeev/criminalintent/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -425,6 +426,17 @@ public class CrimeFragment extends Fragment {
         //предаем установленный формат в текст кнопки
         mTimeButton.setText( tf.format( mCrime.getDate() ) );
 
+    }
+
+    private void updatePhotoView() {
+        if (mPhotoFile == null || !mPhotoFile.exists()) {
+            mPhotoView.setImageDrawable( null );
+        } else {
+            Bitmap bitmap = PictureUtils.getScaledBitmap(
+                    mPhotoFile.getPath(), getActivity()
+            );
+            mPhotoView.setImageBitmap( bitmap );
+        }
     }
 
 
