d55aa4df8faf38fe71129f969772ad99c08bd571
avdey <avdey87@gmail.com>
2017-10-18 17:52:22 +0300

    342 onCrimeSelected разобрать что делает

--- app/src/main/java/com/aavdeev/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/aavdeev/criminalintent/CrimeListActivity.java
@@ -1,5 +1,6 @@
 package com.aavdeev.criminalintent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
@@ -12,9 +13,28 @@ public class CrimeListActivity extends SingleFragmentActivity implements CrimeLi
     protected int getLayoutResId() {
         return R.layout.activity_masterdatail;
     }
-
+// определить интерфейс какого типа был заполнен
     @Override
     public void onCrimeSelected(Crime crime) {
+        //Проверяем наличие индетификатора в detail_fragment_container
+        //имеется ли у него контейнер detail_fragment_container
+        // для размещения в  CrimeFragment.
+        if (findViewById( R.id.detail_fragment_container ) == null) {
+            Intent intent = CrimePagerActivity.newIntent( this, crime.getId() );
+            startActivity( intent );
+        }
+        //если содержит
+        else {
+            //Создаем CrimeFragment. переменную и помещаем в нее
+            //
+            Fragment newDetail = CrimeFragment.newInstance( crime.getId() );
+            getSupportFragmentManager().beginTransaction()
+                    //помещаем в detail_fragment_container фрагмент который хотим видеть
+                    .replace( R.id.detail_fragment_container, newDetail )
+                    .commit();
+
+        }
 
     }
+
 }
